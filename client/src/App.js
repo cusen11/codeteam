435d0fe5dbc8bb075f86586e84c9f126751c4f1a
hellgray98 <henrynguyen202@gmail.com>
2022-10-26 11:55:43 +0700

    create new button green and gray

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ function App() {
       <Button classNumType="button-primary" text="Button Primary" />
       <Button classNumType="button-danger" text="Button Danger" />
       <Button classNumType="button-warning" text="Button Warning" />
+      <Button classNumType="button-green" text="Button Green" />
+      <Button classNumType="button-gray" text="Button Gray" />
       <br/>
       <Links href="abc.com"/>
       <Links href="abc1.com"/>
