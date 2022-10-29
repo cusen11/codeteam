96ff471af3e78e9f332e822ef3c32a2ecbf4c52b
NguyenDinhThanh <dinhthanh1997.work@gmail.com>
2022-10-29 16:16:06 +0700

    Add Component Input

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Button from "./Component/Button/Button";
+import Input from "./Component/Input/Input";
 
 function App() {
   return (
@@ -6,6 +7,9 @@ function App() {
       <Button classNumType="button-primary" text="Button Primary" />
       <Button classNumType="button-danger" text="Button Danger" />
       <Button classNumType="button-warning" text="Button Warning" />
+      <Input labelName="Username" htmlFor="user-name" inputType="text" placeHolder="Username" className="form-control"/>
+      <Input labelName="Password" htmlFor="password" inputType="password" placeHolder="Password" className="form-control"/>
+      <Input inputType="submit" className="form-submit"/>
     </>
   );
 }
